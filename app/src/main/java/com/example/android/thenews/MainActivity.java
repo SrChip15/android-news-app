2f56a9c7f776ca1101077d41c76ab3e54eef916a
k3rn3l16 <k3rn3l16@outlook.com>
2018-02-17 22:11:54 -0800

    Add toolbar as app bar
    Status bar is now grey in color after removing action bar

--- app/src/main/java/com/example/android/thenews/MainActivity.java
+++ app/src/main/java/com/example/android/thenews/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.android.thenews;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 	@Override
@@ -12,11 +15,20 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		Context context = getApplicationContext();
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		toolbar.setTitle(getString(R.string.app_name));
+		toolbar.setTitleTextColor(ContextCompat.getColor(context, android.R.color.white));
+		setSupportActionBar(toolbar);
+
 		// Find the view pager that will allow the user to swipe between fragments
 		ViewPager viewPager = findViewById(R.id.viewpager);
 
 		// Give the TabLayout the ViewPager
 		TabLayout tabLayout = findViewById(R.id.sliding_tabs);
+		tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(context, R.color.colorAccent));
+		tabLayout.setTabTextColors(ContextCompat.getColor(context, android.R.color.white),
+		ContextCompat.getColor(context, android.R.color.black));
 		tabLayout.setupWithViewPager(viewPager);
 
 		// Create an adapter that knows which fragment should be shown on each page
