deedc924aede61017ea4359131849dbd6195485a
k3rn3l16 <k3rn3l16@outlook.com>
2018-02-18 00:02:19 -0800

    Add menu item and icon for refresh
    Yet to functionalize the refresh menu

--- app/src/main/java/com/example/android/thenews/MainActivity.java
+++ app/src/main/java/com/example/android/thenews/MainActivity.java
@@ -8,32 +8,42 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
-	@Override
-	protected void onCreate(@Nullable Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		Toolbar toolbar = findViewById(R.id.toolbar);
-		toolbar.setTitle(getString(R.string.app_name));
-		setSupportActionBar(toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.app_name));
+        setSupportActionBar(toolbar);
 
-		// Find the view pager that will allow the user to swipe between fragments
-		ViewPager viewPager = findViewById(R.id.viewpager);
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = findViewById(R.id.viewpager);
 
-		// Give the TabLayout the ViewPager
-		Context context = getApplicationContext();
-		TabLayout tabLayout = findViewById(R.id.sliding_tabs);
-		tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(context, R.color.colorAccent));
-		tabLayout.setTabTextColors(ContextCompat.getColor(context, android.R.color.white),
-		ContextCompat.getColor(context, android.R.color.black));
-		tabLayout.setupWithViewPager(viewPager);
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = findViewById(R.id.sliding_tabs);
 
-		// Create an adapter that knows which fragment should be shown on each page
-		NewsCategoryAdapter adapter = new NewsCategoryAdapter(getSupportFragmentManager());
+        Context context = getApplicationContext();
+        tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(context, R.color.colorAccent));
+        tabLayout.setTabTextColors(
+                ContextCompat.getColor(context, android.R.color.white),
+                ContextCompat.getColor(context, android.R.color.black)
+        );
+        tabLayout.setupWithViewPager(viewPager);
 
-		// Set the adapter onto the view pager
-		viewPager.setAdapter(adapter);
-	}
+        // Create an adapter that knows which fragment should be shown on each page
+        NewsCategoryAdapter adapter = new NewsCategoryAdapter(getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
 }
