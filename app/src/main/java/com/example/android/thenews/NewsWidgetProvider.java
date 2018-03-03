b9daa1c695bc622d2de573aa162ac431875dd093
k3rn3l16 <k3rn3l16@outlook.com>
2018-03-03 11:28:29 -0800

    Add widget content and set click event

--- app/src/main/java/com/example/android/thenews/NewsWidgetProvider.java
+++ app/src/main/java/com/example/android/thenews/NewsWidgetProvider.java
@@ -1,8 +1,10 @@
 package com.example.android.thenews;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 /** Implementation of App Widget functionality. */
@@ -11,10 +13,21 @@ public class NewsWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Get sample news story strings
+        CharSequence sampleNewStoryOne = context.getString(R.string.sample_news_story_1);
+        CharSequence sampleNewStoryTwo = context.getString(R.string.sample_news_story_2);
+        CharSequence sampleNewStoryThree = context.getString(R.string.sample_news_story_3);
+
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.news_widget_provider);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.news_widget);
+        views.setTextViewText(R.id.appwidget_tv_1, sampleNewStoryOne);
+        views.setTextViewText(R.id.appwidget_tv_2, sampleNewStoryTwo);
+        views.setTextViewText(R.id.appwidget_tv_3, sampleNewStoryThree);
+
+        // Widget click open app
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.news_widget_container, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
